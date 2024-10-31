de656af7ac025a723a8e6403c79a1bd9ba24112f
Nahom Berhane <nahomba@stud.ntnu.no>
2024-10-31 21:41:21 +0100

    saksbehandler classe lagt til

---
+++ src/main/java/no/nav/saksbehandler/Saksbehandler.java
@@ -0,0 +1,23 @@
+package no.nav.saksbehandler;
+
+import no.nav.dagpenger.DagpengerKalkulator;
+
+public class Saksbehandler {
+    public enum muligUtfall {
+        AVSLAG,
+        INNVILGET,
+        INNVILGET_MEDMAKS_SATS
+    };
+
+    
+    
+    public void BehanldeResultat(DagpengerKalkulator dagpengerKalkulator){
+        /**
+         * 
+         * switch(dagpengerKalkulator.regn)
+         *  AVSLAG? 
+         * INNVILGET?
+         * 
+         */
+    }
+}
