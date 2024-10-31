6b19097282b831c4678fc8ec01d448aba675f2d1
Nahom Berhane <nahomba@stud.ntnu.no>
2024-10-31 20:34:30 +0100

    håndtere at data og lønn er gyldig og litt mere DOXYGEN på årslønn .Årslønn

--- src/main/java/no/nav/årslønn/Årslønn.java
+++ src/main/java/no/nav/årslønn/Årslønn.java
@@ -1,5 +1,8 @@
 package no.nav.årslønn;
 
+
+import java.time.LocalDate;
+
 /**
  * Representeren en person sin lønn et kalenderår.
  * Holder på informasjon som hvilket år lønnen tilhører, og selve lønnen det kalenderåret.
@@ -12,9 +15,21 @@ public class Årslønn {
     private final int åretForLønn;
     private final double årslønn;
 
+   /**
+   * Oppretter et Årslønn-objekt med et spesifisert år og årslønn.
+   * @param åretForLønn året lønnen tilhører.
+   * @param årslønn årslønnen.
+   */
     public Årslønn(int åretForLønn, double årslønn) {
-        this.åretForLønn = åretForLønn;
-        this.årslønn = årslønn;
+        //se om år og lønn er gydlig før vi initalisere  
+        if (åretForLønn < 1899 || åretForLønn > LocalDate.now().getYear()) {
+            throw new IllegalArgumentException("Året må være gyldig.");
+        }
+        if (årslønn < 0) {
+          throw new IllegalArgumentException("Årslønn kan ikke være negativ.");
+        }
+       this.åretForLønn = åretForLønn;
+       this.årslønn = årslønn;
     }
 
     /**
@@ -22,7 +37,7 @@ public class Årslønn {
      * @return året for lønnen.
      */
     public int hentÅretForLønn() {
-        return åretForLønn;
+        return this.åretForLønn;
     }
 
     /**
@@ -30,7 +45,7 @@ public class Årslønn {
      * @return årslønnen.
      */
     public double hentÅrslønn() {
-        return årslønn;
+        return this.årslønn;
     }
 
 }
